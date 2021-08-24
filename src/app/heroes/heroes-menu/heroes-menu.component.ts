26b276563d7d1b3f2abec6944a77e86b1bdf2b9e
fvanderflier <74768405+vanderFlierFilip@users.noreply.github.com>
2021-08-24 14:38:04 +0200

    changed import statements from actions in two hero components and edited README

--- src/app/heroes/heroes-menu/heroes-menu.component.ts
+++ src/app/heroes/heroes-menu/heroes-menu.component.ts
@@ -1,14 +1,11 @@
 import * as fromStore from '../store/reducers/heroes.reducer';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Hero } from '../../shared/models/hero';
-import { HeroesService } from '../services/heroes.service';
 import * as fromSelector from './../store/heroes.selectors';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { loadHeroes } from '../store/actions/heroes.actions';
-
+import * as fromActions from '@ng-heroes/heroes/store/actions/heroes.actions';
 @Component({
   selector: 'hrs-heroes-menu',
   templateUrl: './heroes-menu.component.html',
@@ -17,18 +14,18 @@ import { loadHeroes } from '../store/actions/heroes.actions';
 export class HeroesMenuComponent implements OnInit {
   topHeroes$?: Observable<Hero[]>;
   isLoading$?: Observable<boolean>;
-  constructor(
-    private heroesService: HeroesService,
-    private store: Store<fromStore.HeroesState>
-  ) {}
+
+  constructor(private store: Store<fromStore.HeroesState>) {}
 
   ngOnInit(): void {
     this.getTopHeroes();
+
     this.isLoading$ = this.store.select(fromSelector.loading);
   }
 
   getTopHeroes(): void {
-    this.store.dispatch(loadHeroes());
+    this.store.dispatch(fromActions.loadHeroes());
+
     this.topHeroes$ = this.store
       .select(fromSelector.heroesSelector)
       .pipe(map((heroesState) => heroesState.heroList.slice(1, 5)));
